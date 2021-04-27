7f7753d1ad701b105e3094f686a3b79c9959f1dc
Akhil Srivastava <akhilsrivastava193@gmail.com>
2021-04-27 11:00:27 +0530

    SQL File updated

--- logintest.sql
+++ logintest.sql
@@ -3,19 +3,13 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2021 at 05:45 PM
+-- Generation Time: Apr 27, 2021 at 07:29 AM
 -- Server version: 10.4.18-MariaDB
 -- PHP Version: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `logintest`
@@ -28,17 +22,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `mstlogin` (
-  `loginid` varchar(50) NOT NULL,
+  `loginid` int(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
   `name` varchar(150) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `role` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `mstlogin`
 --
 
-INSERT INTO `mstlogin` (`loginid`, `name`, `password`) VALUES
-('akhil', 'Akhil Srivastava', '123');
+INSERT INTO `mstlogin` (`loginid`, `email`, `name`, `password`, `role`) VALUES
+(1, 'akhilsrivastava193@gmail.com', 'Akhil Srivastava', '123', 0),
+(3, 'test', 'akhil', '123', 1),
+(4, 'jgk', 'mjhjhk', '12', 1);
 
 --
 -- Indexes for dumped tables
@@ -49,8 +47,14 @@ INSERT INTO `mstlogin` (`loginid`, `name`, `password`) VALUES
 --
 ALTER TABLE `mstlogin`
   ADD PRIMARY KEY (`loginid`);
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mstlogin`
+--
+ALTER TABLE `mstlogin`
+  MODIFY `loginid` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
